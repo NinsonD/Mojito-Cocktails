e7e9c5103db4f54cbfab0b8ecb1fb62e987d8ca2
NinsonD <godclutch958@gmail.com>
2025-07-24 15:16:07 +0400

    Add initial project structure with constants, images, and navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import NavBar from "./components/NavBar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      <h1 className="flex-center h-[100vh]">Hello</h1>
-    </div>
+    <main>
+        <NavBar />
+    </main>
   );
 };
 
